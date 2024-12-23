be558617794f499cd29c844aec8d39a1ca3b74c9
Oliver Huang <oliver.h0803@gmail.com>
2024-12-23 15:42:20 -0500

    added descriptions

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Home.module.css'; // Include custom CSS
+import '../styles/globals.css'; // Include global CSS
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
